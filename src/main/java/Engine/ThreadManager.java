457037cf11cf61cd12dc3aa9960f5f22134b888a
TaytraX <gregoirengilot@gmail.com>
2025-06-26 21:46:24 -0400

    gdf

--- src/main/java/Engine/ThreadManager.java
+++ src/main/java/Engine/ThreadManager.java
@@ -1,6 +1,7 @@
 package Engine;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadManager {
@@ -8,6 +9,22 @@ public class ThreadManager {
     public static ReentrantLock playerlock = new ReentrantLock();
     public static ReentrantLock logiclock = new ReentrantLock();
 
-    public static ExecutorService logicExecutor;
-    public static ExecutorService playerExecutor;
+    public static ExecutorService worldGenerationExecutor;
+    public static ExecutorService gameplayExecutor;
+
+    public ThreadManager() {
+
+        worldGenerationExecutor = Executors.newSingleThreadExecutor(r -> {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            return thread;
+        });
+
+        gameplayExecutor = Executors.newSingleThreadExecutor(r -> {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            return thread;
+        });
+    }
+
 }
