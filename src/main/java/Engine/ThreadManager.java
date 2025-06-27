c7d63d8ddee5c39baa8858209bfbaa8c89d0d688
TaytraX <gregoirengilot@gmail.com>
2025-06-27 10:08:27 -0400

    gdf

--- src/main/java/Engine/ThreadManager.java
+++ src/main/java/Engine/ThreadManager.java
@@ -1,22 +1,27 @@
 package Engine;
 
+import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadManager {
 
-    public static ReentrantLock playerlock = new ReentrantLock();
-    public static ReentrantLock logiclock = new ReentrantLock();
+    public static final AtomicReference<PlayerState> playerState  = new AtomicReference<>();
 
-    public static ExecutorService worldGenerationExecutor;
-    public static ExecutorService gameplayExecutor;
+    public static final BlockingDeque<WorldGenRequest> worldGenQueue = new LinkedBlockingQueue<>();
+    public static final BlockingDeque<GeneratedPlatforms> generatedPlatforms = new LinkedBlockingQueue<>();
+
+
+    public ExecutorService worldGenerationExecutor;
+    public ExecutorService gameplayExecutor;
 
     public ThreadManager() {
 
         worldGenerationExecutor = Executors.newSingleThreadExecutor(r -> {
             Thread thread = new Thread(r);
-            thread.setDaemon(true);
             return thread;
         });
 
