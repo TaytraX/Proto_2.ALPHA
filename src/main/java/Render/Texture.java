824f4fc6cb29313342807c55a3edc049963e8810
TaytraX <gregoirengilot@gmail.com>
2025-06-28 11:43:11 -0400

    gdf

--- src/main/java/Render/Texture.java
+++ src/main/java/Render/Texture.java
@@ -3,46 +3,42 @@ package Render;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.stb.STBImage;
 import org.lwjgl.system.MemoryStack;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
-import static org.lwjgl.opengl.GL11.glGenTextures;
-import static org.lwjgl.stb.STBImage.stbi_load;
 
 public class Texture {
 
     int textureID;
     int width, height;
-    ByteBuffer pixels;
     private final List<Integer> textures = new ArrayList<>();
 
     public Texture(String filename) throws Exception {
-        try {
-        String texturePath = "src/main/resources/textures/" + filename + ".pnj";
-        textureID = loadTexture(texturePath);
-    } catch (IOException e) {
-        e.printStackTrace();
+        textureID = tryLoadTexture(filename);
+        if (textureID == 0) {
+            textureID = createDefaultTexture();
+        }
     }
 
-        try {
-            String texturePath = "src/main/resources/textures/" + filename + ".jpg";
-            textureID = loadTexture(texturePath);
-    } catch (IOException e) {
-            e.printStackTrace();
-    }
+    private int tryLoadTexture(String filename) {
+        String[] extensions = {".png", ".jpg", ".jpeg"};
+
+        for (String ext : extensions) {
+            try {
+                String path = "src/main/resources/textures/" + filename + ext;
+                return loadTexture(path);
+            } catch (Exception e) {
+                // Continue avec l'extension suivante
+            }
+        }
+        return 0; // Aucune texture chargée
     }
 
 
     private int loadTexture(String filename) throws Exception {
-
-            int width, height;
             ByteBuffer buffer;
 
             try (MemoryStack stack = MemoryStack.stackPush()) {
@@ -55,15 +51,15 @@ public class Texture {
                     throw new Exception("Could not load file " + filename + " " + STBImage.stbi_failure_reason());
                 }
 
-                width = w.get();
-                height = h.get();
+                this.width = w.get();
+                this.height = h.get();
             }
 
             int textureID = GL11.glGenTextures();
             textures.add(textureID);
             GL11.glBindTexture(GL_TEXTURE_2D, textureID);
             GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1);
-            GL11.glTexImage2D(GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
+            GL11.glTexImage2D(GL_TEXTURE_2D, 0, GL11.GL_RGBA, this.width, this.height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
 
             // Paramètres de filtrage
             GL11.glTexParameteri(GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
@@ -74,7 +70,7 @@ public class Texture {
 
     }
 
-    public synchronized int createDefaultTexture() {
+    public int createDefaultTexture() {
         // Texture 2x2 pixels blancs
         ByteBuffer data = org.lwjgl.BufferUtils.createByteBuffer(16);
         for (int i = 0; i < 16; i++) {
@@ -90,10 +86,16 @@ public class Texture {
         GL11.glTexParameteri(GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
         GL11.glTexParameteri(GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
 
+        this.width = 2;
+        this.height = 2;
+
         return textureID;
     }
 
     public void cleanUp(){
-        GL11.glDeleteTextures(textureID);
+
+        for (int texture : textures) {
+            GL11.glDeleteTextures(texture);
+        }
     }
 }
