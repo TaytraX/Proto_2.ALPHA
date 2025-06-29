f96e35c7f0126ed881d757f4b85016a139c96c0f
TaytraX <gregoirengilot@gmail.com>
2025-06-29 18:56:43 -0400

    gdf

--- src/main/java/Engine/AABB.java
+++ src/main/java/Engine/AABB.java
@@ -1,6 +1,15 @@
 package Engine;
 
+import org.joml.Vector2f;
+
 public class AABB {
 
-    public
+    private final Vector2f position;
+    private final Vector2f size;
+
+    public AABB(Vector2f position, Vector2f size) {
+        this.position = position;
+        this.size = size;
+    }
+
 }
