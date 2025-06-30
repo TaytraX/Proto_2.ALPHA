284f70dfdd5ea00b6ce07bae1d104f4d6b927a18
TaytraX <gregoirengilot@gmail.com>
2025-06-29 21:01:03 -0400

    gdf

--- src/main/java/Engine/AABB.java
+++ src/main/java/Engine/AABB.java
@@ -2,13 +2,25 @@ package Engine;
 
 import org.joml.Vector2f;
 
-public record AABB(Vector2f position, Vector2f size) {
+public class AABB {
+
+    Vector2f position, size;
+
+    public AABB(Vector2f position, float width, float height) {
+        this.position = position;
+        this.size = new Vector2f(width, height);
+    }
+
+    public AABB(Vector2f position, Vector2f size) {
+        this.position = position;
+        this.size = size;
+    }
 
     public boolean collidesWith(AABB other) {
-        return getMaxX() >= other.getMinX() &&
-                getMinX() <= other.getMaxX() &&
-                getMaxY() >= other.getMinY() &&
-                getMinY() <= other.getMaxY();
+        return getMaxX() >= (other.getMinX() * 2) &&
+               getMinX() <= (other.getMaxX() * 2) &&
+               getMaxY() >= (other.getMinY() * 2) &&
+               getMinY() <= (other.getMaxY() * 2);
     }
 
     public float getMinX() {
