edddad928b240f8783b320f86f6f21dfd9eaa1bd
TaytraX <gregoirengilot@gmail.com>
2025-06-29 20:01:18 -0400

    gdf

--- src/main/java/Engine/AABB.java
+++ src/main/java/Engine/AABB.java
@@ -2,50 +2,29 @@ package Engine;
 
 import org.joml.Vector2f;
 
-public class AABB {
-
-    private final Vector2f position;
-    private final Vector2f size;
-
-    public AABB(Vector2f position, Vector2f size) {
-        this.position = position;
-        this.size = size;
-    }
-
-    public AABB(Vector2f position, float width, float height) {
-        this.size = new Vector2f(width, height);
-        this.position = position;
-    }
+public record AABB(Vector2f position, Vector2f size) {
 
     public boolean collidesWith(AABB other) {
         return getMaxX() >= other.getMinX() &&
-               getMinX() <= other.getMaxX() &&
-               getMaxY() >= other.getMinY() &&
-               getMinY() <= other.getMaxY();
-    }
-
-    public Vector2f getPosition() {
-        return position;
-    }
-
-    public Vector2f getSize() {
-        return size;
+                getMinX() <= other.getMaxX() &&
+                getMaxY() >= other.getMinY() &&
+                getMinY() <= other.getMaxY();
     }
 
     public float getMinX() {
-        return this.position.x - this.size.x/2;
+        return this.position.x - this.size.x / 2;
     }
 
     public float getMaxX() {
-        return this.position.x + this.size.x/2;
+        return this.position.x + this.size.x / 2;
     }
 
     public float getMinY() {
-        return this.position.y - this.size.y/2;
+        return this.position.y - this.size.y / 2;
     }
 
     public float getMaxY() {
-        return this.position.y + this.size.y/2;
+        return this.position.y + this.size.y / 2;
     }
 
 }
