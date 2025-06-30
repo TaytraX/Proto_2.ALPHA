dae6469664bec844a292b9533af82c9809755096
TaytraX <gregoirengilot@gmail.com>
2025-06-30 12:44:11 -0400

    gdf

--- src/main/java/Engine/Physics.java
+++ src/main/java/Engine/Physics.java
@@ -8,26 +8,20 @@ public class Physics {
     public static final float airFriction = 0.001f;
     public static final float groundFriction = 0.5f;
 
-    public Vector2f position;
-    public Vector2f platforms;
+    public AABB position;
+    public AABB platforms;
     public Vector2f velocity;
     public boolean isground = true;
 
 
-    public Physics(Vector2f position) {
-
-        this.position = new Vector2f(position);
-        this.velocity = new Vector2f(0, 0);
+    public Physics(AABB position, AABB platforms) {
+        this.position = position;
+        this.platforms = platforms;
+        this.velocity = new Vector2f();
 
     }
 
     public void update(float deltaTime) {
-
-        if(!isground) {
-            velocity.y += gravity * deltaTime;
-        }
-        position.x += velocity.x * deltaTime;
-        position.y += velocity.y * deltaTime;
 
     }
 
