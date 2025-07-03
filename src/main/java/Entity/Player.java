9d4031156b7987f1c68a4009569e26ede04b7e40
TaytraX <gregoirengilot@gmail.com>
2025-07-03 18:59:45 -0400

    gdf

--- src/main/java/Entity/Player.java
+++ src/main/java/Entity/Player.java
@@ -1,17 +1,25 @@
 package Entity;
 
 import Engine.AABB;
+import Laucher.Main;
+import Render.Window;
 import org.joml.Vector2f;
 
+import static Laucher.Main.window;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
+import static org.lwjgl.glfw.GLFW.glfwGetKey;
+
 public class Player {
-    public static AABB position = new AABB(new Vector2f(0, 0), new Vector2f(1, 1));
+    private final AABB position = new AABB(new Vector2f(0, 0), new Vector2f(1, 1));
+    public Window window;
 
 
-    public static AABB getposition(){
+    public AABB getposition(){
         return position;
     }
 
     public void input() {
+        glfwGetKey(window, GLFW_KEY_SPACE);
     }
 
     public void update(float deltaTime) {
