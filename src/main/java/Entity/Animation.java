1e52bd1e6799a888012aeacc248310b2d316a8f3
TaytraX <gregoirengilot@gmail.com>
2025-07-06 14:35:54 -0400

    gdf

--- src/main/java/Entity/Animation.java
+++ src/main/java/Entity/Animation.java
@@ -2,26 +2,16 @@ package Entity;
 
 public class Animation {
 
-    public AnimationState animation(PlayerState state) {
+    public AnimationState processTransiteAnimation(){
 
-        boolean isMoveLeft = false, isMoveRight = false, isJump = false, isFalling = false, isStop = false, landing = false, isIdle = false;
+        boolean isMoveLeft = false, isMoveRight = false, isJump = false, isFalling = false, isStop = false, landing = false, isIdle = false, isgrounded = false;
 
-        if (state.moveLeft()) isMoveLeft = true;
-        if (state.moveRight()) isMoveRight = true;
-        if (state.jump()) isJump = true;
-        if (state.isGrounded()) isStop = true;
-        if (state.velocity().y < 0) isFalling = true;
-        if (state.velocity().y == 0) landing = true;
-        if (!isMoveLeft && !isMoveRight && !isJump && !isFalling && !isStop && !landing) isIdle = true;
-
-        return switch (state.animationState()) {
-            case IDLE ->  AnimationState.IDLE;
-            case WALKLEFT -> AnimationState.WALKLEFT;
-            case WALKRIGHT -> AnimationState.WALKRIGHT;
-            case JUMPING -> AnimationState.JUMPING;
-            case FALLING -> AnimationState.FALLING;
-            case BRAKE -> AnimationState.BRAKE;
-            case LANDING -> AnimationState.LANDING;
-        };
+        if(isMoveLeft && isgrounded) return AnimationState.WALKLEFT;
+        if(isMoveRight && isgrounded) return AnimationState.WALKRIGHT;
+        if(isJump) return AnimationState.JUMPING;
+        if(isFalling && isgrounded) return AnimationState.FALLING;
+        if(isStop) return AnimationState.BRAKE;
+        if(landing) return AnimationState.LANDING;
+        return AnimationState.IDLE;
     }
 }
