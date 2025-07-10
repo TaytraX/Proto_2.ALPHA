9eeb0c80fad5fae9af518350781aa6a16a91a17f
TaytraX <gregoirengilot@gmail.com>
2025-07-10 18:28:24 -0400

    bf

--- src/main/java/Engine/Physics.java
+++ src/main/java/Engine/Physics.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Physics {
     public static final float GRAVITY = -12.00f;
-    public static final float GROUND_FRICTION = 5.0f;
+    public static final float GROUND_FRICTION = 5.021f;
 
     public PlayerState update(PlayerState currentState, float deltaTime, List<AABB> platforms) {
         Vector2f newPosition = new Vector2f(currentState.position());
