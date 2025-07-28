e86b065fb5133e9a1fcd0b08cd3d27281467d5c9
TaytraX <gregoirengilot@gmail.com>
2025-07-28 12:24:45 -0400

    gdf

--- src/main/java/Engine/Physics.java
+++ src/main/java/Engine/Physics.java
@@ -6,15 +6,13 @@ import org.joml.Vector2f;
 import java.util.List;
 
 public class Physics {
-    public static final float GRAVITY = -12.00f;
+    public static final float GRAVITY = -8.0f;
+
 
     public PlayerState update(PlayerState currentState,  List<AABB> platforms, float deltaTime) {
         Vector2f newVelocity = new Vector2f(currentState.velocity());
         Vector2f newPosition = new Vector2f(currentState.position());
         boolean isGrounded = false;
-
-        // Calcul de la vitesse max basée uniquement sur la gravité
-        float maxSpeed = (float) Math.sqrt(Math.abs(GRAVITY) * 9.0f) * currentState.force();
 
         // Appliquer la gravité
         newVelocity.y += GRAVITY * deltaTime;
@@ -43,19 +41,18 @@ public class Physics {
             }
         }
 
+
+
+        // Calcul de la vitesse max basée uniquement sur la gravité
+        float maxSpeed = 45.0f / (float) Math.sqrt(Math.abs(GRAVITY));
+
         float impactVelocity = newVelocity.y;
         boolean wasGrounded = currentState.isGrounded();
-
-        // Décélération exponentielle basée sur la gravité
-        if (isGrounded && !currentState.moveLeft() && !currentState.moveRight()) {
-            float frictionRate = Math.abs(GRAVITY) * 0.30f; // 30% de la gravité
-            newVelocity.x *= Math.max(0, 1.0f - frictionRate * deltaTime);
-        }
+        newPosition.x += newVelocity.x * deltaTime;
 
         newVelocity.x = Math.max(-maxSpeed, Math.min(maxSpeed, newVelocity.x));
-        if (newVelocity.x < maxSpeed) {
-            newPosition.x += newVelocity.x * deltaTime;
-        }
+        System.out.println("vitesse de joueur : " + newVelocity.x);
+
         playerAABB = new AABB(newPosition, PlayerState.PLAYER_SIZE);
 
         // 1. ÉTAPE X : Traiter toutes les plateformes pour X
@@ -76,6 +73,12 @@ public class Physics {
                     }
                 }
             }
+        }
+
+        // Décélération exponentielle basée sur la gravité
+        if ((currentState.moveLeft() && newVelocity.x > 0) || (currentState.moveRight() && newVelocity.x < 0)) {
+            float frictionRate = Math.abs(GRAVITY) * 0.30f; // 30% de la gravité
+            newVelocity.x *= Math.max(0, 1.0f - frictionRate * deltaTime);
         }
 
         return new PlayerState(
